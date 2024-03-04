4723b11b51d9dad21f4b7161e2c53d99eb340c8c
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-04 12:07:39 -0300

    add: adiciona filtro que verifica se usuário está logado p ter acesso a rota

---
+++ AdaTech.InventoryControl.WebAPI/Filters/NotLoggedInFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AdaTech.InventoryControl.WebAPI.Filters
+{
+    public class NotLoggedInFilter : IAuthorizationFilter
+    {
+        private IHttpContextAccessor? _HttpContextAccessor { get; set; }
+
+        public NotLoggedInFilter(IHttpContextAccessor httpContextAccessor)
+        {
+            _HttpContextAccessor = httpContextAccessor;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!_HttpContextAccessor!.HttpContext!.Request.Cookies.ContainsKey("JWT"))
+            {
+                context.Result = new ContentResult()
+                {
+                    Content = "Usuário precisa estar logado.",
+                    StatusCode = 401
+                };
+            }
+        }
+    }
+}
