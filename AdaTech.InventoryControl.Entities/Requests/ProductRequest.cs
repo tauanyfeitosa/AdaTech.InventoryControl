487a0bc2a602c1c6445e216040ffadb5d8cd7c1b
Maria Eduarda Sampaio <dudah.s3@gmail.com>
2024-03-05 11:45:51 -0300

    feat: adiciona nome como prop required

--- AdaTech.InventoryControl.Entities/Requests/ProductRequest.cs
+++ AdaTech.InventoryControl.Entities/Requests/ProductRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace AdaTech.InventoryControl.WebAPI.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdaTech.InventoryControl.WebAPI.Requests
 {
     public class ProductRequest
     {
+        [Required]
         public string Name { get; set; }
     }
 }
